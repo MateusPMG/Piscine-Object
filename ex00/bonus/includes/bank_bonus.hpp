1bcb88e8ec0b37c6ec47ed4bb20dde0ab59eac03
MateusPMG <mpmgboss@gmail.com>
2024-07-12 13:35:06 +0100

    nearly finished the bonus for ex00

--- ex00/bonus/includes/bank_bonus.hpp
+++ ex00/bonus/includes/bank_bonus.hpp
@@ -11,20 +11,28 @@ class Bank {
 			private:
 				int id;
 				double value;
-				~Account();
 				Account(int id, double value):id(id),value(value){};
 				Account& operator=(const Account& copy);
 				Account(const Account& copy);
+				~Account();
+				friend class Bank;
 			public:
 				int getID()const;
 				double getValue()const;
 		};
-		
-		std::map<int, Account*> clients;
+
+		std::map<int, Account> clients;
 	public:
 		Bank();
 		~Bank();
 		Bank& operator=(const Bank& copy);
 		Bank(const Bank& copy);
-
+		Account& operator[](size_t id);
+		void createAccount(size_t id, double value);
+		void deleteAccount(size_t id);
+		void deposit(size_t id, double value);
+		void withdraw(size_t id, double value);
+		void loan(size_t id, double value);
+		void getNrClients();
+		void getVault();
 };
--- ex00/bonus/srcs/bank_bonus.cpp
+++ ex00/bonus/srcs/bank_bonus.cpp
@@ -28,3 +28,18 @@ Bank::~Bank(){}
 
 Bank::Bank(const Bank& copy):nr_clients(copy.nr_clients),vault(copy.vault),clients(copy.clients){}
 
+Bank::Account& Bank::operator[](size_t id){
+	return (clients[id]);
+}
+
+void Bank::deleteAccount(size_t id){
+	clients.erase(id);
+}
+
+void Bank::createAccount(size_t id, double value){
+	if (clients.find(id) != clients.end()) {
+			return;
+		}
+	clients[id] = Account(id, value);
+}
+
